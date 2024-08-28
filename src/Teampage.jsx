473c2678ab558265445104f7a59fce345f844600
Timothy Omafe Ameh <omafetimothyameh@gmail.com>
2024-08-28 18:49:13 +0100

    modified the images is teams page

--- src/Teampage.jsx
+++ src/Teampage.jsx
@@ -1,6 +1,6 @@
-import TeamsHero from "./teamePageComponents/teamsHero"
-import Brand from "./teamePageComponents/brand"
-import Team from "./teamePageComponents/team"
+import TeamsHero from "./teamePageComponents/teamsHero.jsx"
+import Brand from "./teamePageComponents/brand.jsx"
+import Team from "./teamePageComponents/team.jsx"
 const Teampage = () => {
    return (
       <div className="teamsPage">
